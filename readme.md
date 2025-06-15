394d05842639f32261aeef404a0a7b9ecfa424b4
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-06-14 20:58:18 -0400

    backuop

---
+++ readme.md
@@ -0,0 +1,11 @@
+# atspi tree visualizer
+
+A simple program to label each accessible element with a red box. This is just a proof of concept for the time being.
+
+
+To run:
+```
+cargo run
+```
+
+![wikipedia homepage with red boxes over each accessible element](./docs/image.png)
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,46 @@
 use atspi::connection::set_session_accessibility;
 use atspi::proxy::accessible::{AccessibleProxy, ObjectRefExt};
-use atspi::{Event, ObjectRef, State, WindowEvents};
+
+use atspi::{DocumentEvents, Event, ObjectRef, State};
+
 use atspi_proxies::proxy_ext::ProxyExt;
 use eframe::egui;
 use egui::{Color32, Rangef, Rect};
+
 use futures::executor::block_on;
 use std::error::Error;
 use std::sync::Arc;
+
 use tokio::sync::mpsc::{self, UnboundedReceiver};
+
 use tokio_stream::StreamExt;
 use zbus::Connection;
 
-fn recursive_collect_children<'a>(
-    proxy: &'a AccessibleProxy<'a>,
-    conn: &'a Connection,
-) -> std::pin::Pin<
-    Box<dyn std::future::Future<Output = Result<Vec<ObjectRef>, Box<dyn Error>>> + Send + 'a>,
-> {
-    Box::pin(async move {
-        let mut collected_refs = Vec::new();
-        for child in proxy.get_children().await?.iter() {
-            let child_proxy = child.as_accessible_proxy(conn).await?;
-            collected_refs.push(child.clone());
+/// Performs a depth-first search to collect children in the accessibility tree.
+async fn dfs_collect_children(
+    root: AccessibleProxy<'_>,
+    conn: &Arc<Connection>,
+) -> Result<Vec<ObjectRef>, Box<dyn Error>> {
+    let mut stack = vec![root];
+    let mut collected = Vec::new();
 
-            
+    while let Some(proxy) = stack.pop() {
+        let children = proxy.get_children().await?;
+
+        for child in children {
+            let child_proxy = child.clone().into_accessible_proxy(&conn).await?;
+
+            stack.push(child_proxy.clone());
+
             let state = block_on(child_proxy.get_state())?;
 
-            let is_link = state.contains(State::Showing);
-
-            let mut child_refs = recursive_collect_children(&child_proxy, conn).await?;
-            if is_link {
-                collected_refs.append(&mut child_refs);
+            if state.contains(State::Showing) {
+                collected.push(child);
             }
         }
-
-        Ok(collected_refs)
-    })
+    }
+    println!("Collected {} children", collected.len());
+    Ok(collected)
 }
 
 #[tokio::main]
@@ -51,7 +56,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .with_decorations(false)
             .with_mouse_passthrough(true)
             .with_fullscreen(true),
-
         ..Default::default()
     };
 
@@ -60,11 +64,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     eframe::run_native(
         "Atspi Visualizer",
         options,
-        Box::new({
+        Box::new({ 
             let atspi = atspi.clone();
             let conn = conn.clone();
             move |cc| {
                 let frame = cc.egui_ctx.clone();
+
                 tokio::spawn(async move {
                     frame.request_repaint();
                 });
@@ -74,34 +79,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 let tx_gui_clone = tx_gui.clone();
 
                 tokio::spawn(async move {
-                    atspi_clone.register_event::<WindowEvents>().await.unwrap();
+                    atspi_clone.register_event::<DocumentEvents>().await.unwrap();
                     let mut events = atspi_clone.event_stream();
 
                     while let Some(event) = events.next().await {
                         match event {
-                            Ok(Event::Window(WindowEvents::Activate(ev))) => {
+                            Ok(Event::Document(DocumentEvents::LoadComplete(ev))) => {
                                 let conn_inner = conn_clone.clone();
                                 let tx_inner = tx_gui_clone.clone();
+
                                 tokio::spawn(async move {
-                                    let a11y_proxy =
-                                        ev.item.into_accessible_proxy(&conn_inner).await;
-                                    match a11y_proxy {
-                                        Ok(proxy) => {
-                                            match recursive_collect_children(&proxy, &conn_inner)
-                                                .await
-                                            {
-                                                Ok(object_refs) => {
-                                                    if let Err(err) = tx_inner.send(object_refs) {
-                                                        eprintln!(
-                                                            "Error sending object refs: {err}"
-                                                        );
-                                                    }
-                                                }
-                                                Err(err) => eprintln!("Error: {err}"),
-                                            }
-                                        }
-                                        Err(err) => eprintln!("Error creating proxy: {err}"),
-                                    }
+                                   let a11y_proxy = ev.item.into_accessible_proxy(&conn_inner).await;
+                                   match a11y_proxy {
+                                      Ok(proxy) => {
+                                         match dfs_collect_children(proxy, &conn_inner).await {
+                                             Ok(object_refs) => {
+                                                 if let Err(err) = tx_inner.send(object_refs) {
+                                                     eprintln!("Error sending object refs: {err}")
+                                                 }
+                                             }
+                                             Err(err) => eprintln!("Error collecting children: {err}"),
+                                         }
+                                      }
+                                      Err(err) => eprintln!("Error creating proxy: {err}"),
+                                   }
                                 });
                             }
                             Ok(_) => println!("Other event"),
@@ -140,10 +141,7 @@ impl eframe::App for ScreenPainterGUI {
     }
 
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-        ctx.send_viewport_cmd(egui::ViewportCommand::WindowLevel(
-            egui::WindowLevel::AlwaysOnTop,
-        ));
-
+        ctx.send_viewport_cmd(egui::ViewportCommand::WindowLevel(egui::WindowLevel::AlwaysOnTop));
 
         if let Ok(points) = self.points.try_recv() {
             if !points.is_empty() {
@@ -156,40 +154,39 @@ impl eframe::App for ScreenPainterGUI {
             .show(ctx, |ui| {
                 if let Some(state) = &self.state {
                     println!("Rendering points: {}", state.len());
-                    state.iter().for_each(|point| {
-                        let a11y_proxy = block_on(point.as_accessible_proxy(&self.conn));
-                        match a11y_proxy {
-                            Ok(proxy) => {
-                                if let Ok(proxies) = block_on(proxy.proxies()) {
-                                    if let Ok(component) = block_on(proxies.component()) {
-                                        if let Ok((x0, y0, _, _)) = block_on(
-                                            component.get_extents(atspi::CoordType::Screen),
-                                        ) {
-                                            let painter = ui.painter();
-                                            let x_range = Rangef::new(x0 as f32, (x0 as f32) + 10.0);
-                                            let y_range = Rangef::new(y0 as f32, (y0 as f32) + 10.0);
 
-                                            painter.rect_filled(
-                                                Rect::from_x_y_ranges(x_range, y_range),
-                                                0,
-                                                Color32::RED,
-                                            );
+                    use futures::stream::StreamExt;
 
-                                        } else {
-                                            eprintln!(
-                                                "Error: Failed to get extents from component"
-                                            );
-                                        }
-                                    } else {
-                                        eprintln!("Error: Failed to get component from proxies");
-                                    }
-                                } else {
-                                    eprintln!("Error: Failed to get proxies from proxy");
-                                }
+                    let stream = futures::stream::iter(state.iter()).for_each_concurrent(None, |point| {
+                        let conn = self.conn.clone();
+                        let painter = ui.painter();
+
+                        async move {
+                            match point.as_accessible_proxy(&conn).await {
+                                Ok(proxy) => match proxy.proxies().await {
+                                   Ok(proxies) => match proxies.component().await {
+                                      Ok(component) => match component.get_extents(atspi::CoordType::Screen).await {
+                                         Ok((x0, y0, _, _)) => {
+                                             let x_range = Rangef::new(x0 as f32, (x0 as f32) + 10.0);
+                                             let y_range = Rangef::new(y0 as f32, (y0 as f32) + 10.0);
+                                             painter.rect_filled(
+                                                 Rect::from_x_y_ranges(x_range, y_range),
+                                                 0,
+                                                 Color32::RED,
+                                             );
+                                         }
+                                         Err(err) => eprintln!("Error: Failed to get extents from component: {err}"),
+                                      },
+                                      Err(err) => eprintln!("Error: Failed to get component from proxies: {err}"),
+                                   },
+                                   Err(err) => eprintln!("Error: Failed to get proxies from proxy: {err}"),
+                                },
+                                Err(err) => eprintln!("Error: Failed to create AccessibleProxy: {err}"),
                             }
-                            Err(err) => println!("Error creating proxy: {err}"),
                         }
                     });
+
+                    block_on(stream);
                 }
             });
     }
